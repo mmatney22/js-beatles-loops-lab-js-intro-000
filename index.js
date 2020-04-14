22b4c3cdf7d66ac89c64e2f2f1232811c865f52a
Morgan Matney <morgan.matney89@gmail.com>
2020-04-14 20:44:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function theBeatlesPlay(mus, inst) {
   }
   return foo;
 }
+
+cons facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and a boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  
+}
