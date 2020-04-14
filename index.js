3c7ff1cadab2d1ca5127ee2bc8d3c375fe33f00b
Morgan Matney <morgan.matney89@gmail.com>
2020-04-14 21:16:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ function johnLennonFacts(facts) {
 }
 
 function iLoveTheBeatles(num) {
-  const foo = [];
+  var strings = [];
 
   do {
-    foo.push("I love the Beatles!");
+    strings.push('I love the Beatles!');
     num++
-  } while { num < 15};
-  return foo
+  } while (num < 15);
+  return strings
 }
